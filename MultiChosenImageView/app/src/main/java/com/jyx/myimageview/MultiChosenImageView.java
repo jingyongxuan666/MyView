a9dd230f2d3932205b8736f03445c20cd4bee85b
jingyongxuan666 <jyx5323@163.com>
2019-04-30 00:22:26 +0800

    增加重设尺寸resize方法 增加设置默认值

--- MultiChosenImageView/app/src/main/java/com/jyx/myimageview/MultiChosenImageView.java
+++ MultiChosenImageView/app/src/main/java/com/jyx/myimageview/MultiChosenImageView.java
@@ -18,9 +18,9 @@ import android.widget.RelativeLayout;
 public class MultiChosenImageView extends RelativeLayout {
 
     /**
-     * 长高模式
+     * 宽高模式
      */
-    public static final String SHAPE_MODE_NORMAL = "0";//指定长高
+    public static final String SHAPE_MODE_NORMAL = "0";//指定宽高
     public static final String SHAPE_MODE_EQUALS_WIDTH = "1";//边长为宽的正方形
     public static final String SHAPE_MODE_EQUALS_HEIGHT = "2";//边长为高的正方形
 
@@ -36,6 +36,8 @@ public class MultiChosenImageView extends RelativeLayout {
     public static final String CHOSE_FROM_CAMERA = "0";//拍照
     public static final String CHOSE_FROM_GALLERY = "1";//相册
     public static final String CHOSE_FROM_BOTH = "2";//皆可
+
+    private static final int paddingSizeDp = 10;
 
 
     private ImageView ivMain;
@@ -83,35 +85,21 @@ public class MultiChosenImageView extends RelativeLayout {
     }
 
     public MultiChosenImageView(Context context, AttributeSet attrs) {
-        this(context,attrs,0);
+        this(context, attrs, 0);
     }
 
     public MultiChosenImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         //绑定view
-        LayoutInflater.from(context).inflate(R.layout.muti_chosen_image_view,this,true);
+        LayoutInflater.from(context).inflate(R.layout.muti_chosen_image_view, this, true);
         ivMain = findViewById(R.id.iv_main);
         ivPlay = findViewById(R.id.iv_play);
         ivDelete = findViewById(R.id.iv_delete);
         rlBack = findViewById(R.id.rl_back);
-        ViewGroup viewGroup = (ViewGroup) getParent();
-        //重新调整大小，来补全padding的10dp
-//        DpPxUtils.init(context);
-//        final int paddingSize = DpPxUtils.dipToPx(10);
-//        post(new Runnable() {
-//            @Override
-//            public void run() {
-//                Class<? extends ViewGroup.LayoutParams> viewClass = getLayoutParams().getClass();
-//                try {
-//                    setLayoutParams(viewClass.getDeclaredConstructor(int.class,int.class).newInstance(getWidth(),getWidth()));
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
 
+        DpPxUtils.init(context);
         //获取属性
-        TypedArray attribute = context.obtainStyledAttributes(attrs,R.styleable.MultiChosenImageView);
+        TypedArray attribute = context.obtainStyledAttributes(attrs, R.styleable.MultiChosenImageView);
 
         shapeMode = attribute.getString(R.styleable.MultiChosenImageView_shapeMode);
 
@@ -119,15 +107,69 @@ public class MultiChosenImageView extends RelativeLayout {
 
         choseFrom = attribute.getString(R.styleable.MultiChosenImageView_choseFrom);
 
-        deletable = attribute.getBoolean(R.styleable.MultiChosenImageView_deletable,true);
+        deletable = attribute.getBoolean(R.styleable.MultiChosenImageView_deletable, true);
 
         defaultImage = attribute.getDrawable(R.styleable.MultiChosenImageView_defaultImage);
 
-        limitedSize = attribute .getInt(R.styleable.MultiChosenImageView_limitedSize,2048);
+        limitedSize = attribute.getInt(R.styleable.MultiChosenImageView_limitedSize, 2048);
 
-        //重新调整大小
+        setDefaultValue();
+
+        resize(shapeMode);
 
 
+    }
+
+    /**
+     * 设置各属性默认值
+     */
+    private void setDefaultValue() {
+        if (shapeMode == null)//默认指定宽高
+            shapeMode = SHAPE_MODE_NORMAL;
+
+        if (choseType == null)//默认选择图片
+            choseType = CHOSE_TYPE_IMAGE;
+
+        if (choseFrom == null)//默认从相册选择
+            choseFrom = CHOSE_FROM_GALLERY;
+
+    }
+
+    /**
+     * 重新调整大小
+     *
+     * @param shapeMode
+     */
+    private void resize(final String shapeMode) {
+
+
+        final int paddingSizePx = DpPxUtils.dipToPx(paddingSizeDp);
+        post(new Runnable() {
+            @Override
+            public void run() {
+                Class<? extends ViewGroup.LayoutParams> viewClass = getLayoutParams().getClass();
+                int width;
+                int height;
+                switch (shapeMode) {
+                    case SHAPE_MODE_EQUALS_WIDTH:
+                        height = width = getWidth();
+                        break;
+                    case SHAPE_MODE_EQUALS_HEIGHT:
+                        width = height = getHeight();
+                        break;
+                    default:
+                        width = getWidth() + paddingSizePx;
+                        height = getHeight() + paddingSizePx;
+                        break;
+                }
+
+                try {
+                    setLayoutParams(viewClass.getDeclaredConstructor(int.class,int.class).newInstance(width,height));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
 
     }
