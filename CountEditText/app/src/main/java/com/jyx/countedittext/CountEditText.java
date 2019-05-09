24be4984b4b9880cba867f4da9fd8959d30e129e
jingyongxuan <jingyongxuan@jiemian.com>
2019-05-09 15:51:41 +0800

    解决和scrollview等view的滑动冲突问题

--- CountEditText/app/src/main/java/com/jyx/countedittext/CountEditText.java
+++ CountEditText/app/src/main/java/com/jyx/countedittext/CountEditText.java
@@ -9,6 +9,8 @@ import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.EditText;
 
 /**
  * 可计数的EditText
@@ -59,12 +61,12 @@ public class CountEditText extends AppCompatEditText implements TextWatcher {
 
     public CountEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.CountEditText);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CountEditText);
 
-        mLimitedCount = typedArray.getInt(R.styleable.CountEditText_limitedCounts,100);
+        mLimitedCount = typedArray.getInt(R.styleable.CountEditText_limitedCounts, 100);
         mLeftHintText = typedArray.getString(R.styleable.CountEditText_leftCountHintText);
-        mLeftHintTextSize = (int) typedArray.getDimension(R.styleable.CountEditText_leftCountHintTextSize,getTextSize());
-        mLeftHintTextColor = typedArray.getColor(R.styleable.CountEditText_leftCountHintTextColor,getCurrentHintTextColor());
+        mLeftHintTextSize = (int) typedArray.getDimension(R.styleable.CountEditText_leftCountHintTextSize, getTextSize());
+        mLeftHintTextColor = typedArray.getColor(R.styleable.CountEditText_leftCountHintTextColor, getCurrentHintTextColor());
 
         init();
     }
@@ -72,15 +74,15 @@ public class CountEditText extends AppCompatEditText implements TextWatcher {
     private void init() {
         mPaint = new Paint();
         mLeftCount = mLimitedCount;
-        if (mLeftHintText == null){
-            mLeftHintText = "%d/"+mLeftCount;
+        if (mLeftHintText == null) {
+            mLeftHintText = "%d/" + mLeftCount;
         }
         setFilters(new InputFilter[]{new InputFilter.LengthFilter(mLimitedCount)});
         addTextChangedListener(this);
         //防止输入的文字和剩余文字数量提示信息重合，底部需预留空间
         mPaddingBottom = getPaddingBottom();//获取已设置的paddingBottom
         int textHeight = (int) ((mLeftHintTextSize + 0.00000007) / 0.7535);//文字高度和textSize的关系
-        setPadding(getPaddingLeft(),getPaddingTop(),getPaddingRight(),mPaddingBottom + textHeight + 10 );
+        setPadding(getPaddingLeft(), getPaddingTop(), getPaddingRight(), mPaddingBottom + textHeight + 10);
     }
 
     @Override
@@ -97,7 +99,29 @@ public class CountEditText extends AppCompatEditText implements TextWatcher {
         加上getScrollY为了文字超出高度可滚动时，防止剩余文字数量的提示也跟随滚动，
         再减去设置的paddingBottom值*/
         float y = mPaint.ascent() + getHeight() + getScrollY() - mPaddingBottom;
-        canvas.drawText(String.format(mLeftHintText,mLeftCount), x, y, mPaint);
+        canvas.drawText(String.format(mLeftHintText, mLeftCount), x, y, mPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getActionMasked();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                getParent().requestDisallowInterceptTouchEvent(true);//按下的时候父类不拦截
+                break;
+            case MotionEvent.ACTION_UP:
+                getParent().requestDisallowInterceptTouchEvent(false);//抬起的时候父类拦截
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //判断是否可滑动，如果可滑动则父类不拦截事件，否则拦截
+                if (canVerticalScroll(this)){
+                    getParent().requestDisallowInterceptTouchEvent(true);
+                }else {
+                    getParent().requestDisallowInterceptTouchEvent(false);
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 
     @Override
@@ -114,8 +138,8 @@ public class CountEditText extends AppCompatEditText implements TextWatcher {
     public void afterTextChanged(Editable s) {
         if (getText() != null) {
             mLeftCount = mLimitedCount - getText().length();
-            if (mLeftCount == 0){
-                if (mOnTextOverLimitedListener != null){
+            if (mLeftCount == 0) {
+                if (mOnTextOverLimitedListener != null) {
                     mOnTextOverLimitedListener.overLimited();
                 }
                 return;
@@ -124,11 +148,33 @@ public class CountEditText extends AppCompatEditText implements TextWatcher {
         }
     }
 
-    public void setOnTextOverLimitedListener(OnTextOverLimitedListener onTextOverLimitedListener){
+    /**
+     * EditText竖直方向是否可以滚动
+     * @param editText  需要判断的EditText
+     * @return  true：可以滚动   false：不可以滚动
+     */
+    private boolean canVerticalScroll(EditText editText) {
+        //滚动的距离
+        int scrollY = editText.getScrollY();
+        //控件内容的总高度
+        int scrollRange = editText.getLayout().getHeight();
+        //控件实际显示的高度
+        int scrollExtent = editText.getHeight() - editText.getCompoundPaddingTop() -editText.getCompoundPaddingBottom();
+        //控件内容总高度与实际显示高度的差值
+        int scrollDifference = scrollRange - scrollExtent;
+
+        if(scrollDifference == 0) {
+            return false;
+        }
+        return (scrollY > 0) || (scrollY < scrollDifference - 1);
+    }
+
+    public void setOnTextOverLimitedListener(OnTextOverLimitedListener onTextOverLimitedListener) {
         mOnTextOverLimitedListener = onTextOverLimitedListener;
     }
 
-    public interface OnTextOverLimitedListener{//用于超限后的回调
+    public interface OnTextOverLimitedListener {//用于超限后的回调
+
         void overLimited();
     }
 }
